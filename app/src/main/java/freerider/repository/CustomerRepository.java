9e831f828fbf1ff33d1c334058e995f5a458544d
SicktorHass <igor.work@outlook.de>
2022-01-21 13:36:11 +0100

    forgot add repository

---
+++ app/src/main/java/freerider/repository/CustomerRepository.java
@@ -0,0 +1,11 @@
+package freerider.repository;
+
+import org.springframework.stereotype.Repository;
+import freerider.datamodel.Customer;
+
+@Repository
+public interface CustomerRepository extends
+    org.springframework.data.repository.CrudRepository<Customer, Long>
+{
+
+}
