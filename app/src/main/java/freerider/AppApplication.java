c88d83fb8a546f363363d93996864f31db839e92
SicktorHass <igor.work@outlook.de>
2022-01-21 13:31:40 +0100

    c3&c4 added entity wiring and let mock data use the db

--- app/src/main/java/freerider/AppApplication.java
+++ app/src/main/java/freerider/AppApplication.java
@@ -3,17 +3,19 @@ package freerider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.event.EventListener;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 import freerider.datamodel.Customer;
 import freerider.repository.CustomerRepository;
 
+@EnableJpaRepositories(basePackages = {"freerider.repository"})
+@EntityScan(basePackages = {"freerider.datamodel"})
+@ComponentScan(basePackages = {"freerider.restapi"})
 @SpringBootApplication
-@ComponentScan(
-	basePackages = {"freerider.restapi", "freerider.repository"}
-	)
 public class AppApplication {
 
 	@Autowired
@@ -26,20 +28,22 @@ public class AppApplication {
 	@EventListener( ApplicationReadyEvent.class )
 	public void runAfterSpringStartup() {
 		// runs when Spring is ready
-		customerRepository.save( new Customer()
-			.setId(1).setName( "Eric", "Meyer" ).addContact( "eric98@yahoo.com" )
-			.addContact( "(030) 7000‐640000" )
-			// updated phone number
-		);
-		customerRepository.save( new Customer()
-			.setId(2).setName( "Anne", "Bayer" ).addContact( "anne24@yahoo.de" )
-			.addContact( "(030) 3481‐23352" )
-		);
-		customerRepository.save( new Customer()
-			.setId(3).setName( "Tim", "Schulz‐Mueller" ).addContact( "tim2346@gmx.de" )
-		);
+		if(customerRepository.count()== 0){
+			customerRepository.save( new Customer()
+				.setId(1).setName( "Eric", "Meyer" ).addContact( "eric98@yahoo.com" )
+				.addContact( "(030) 7000‐640000" )
+				// updated phone number
+			);
+			customerRepository.save( new Customer()
+				.setId(2).setName( "Anne", "Bayer" ).addContact( "anne24@yahoo.de" )
+				.addContact( "(030) 3481‐23352" )
+			);
+			customerRepository.save( new Customer()
+				.setId(3).setName( "Tim", "Schulz‐Mueller" ).addContact( "tim2346@gmx.de" )
+			);
+			// 3 customers added to repository
+		}
 		long count = customerRepository.count();
-		// 3 customers added to repository
 		System.out.println( "repository<Customer> with: " + count + " entries" );
 	}
 }
--- app/src/main/java/freerider/datamodel/Customer.java
+++ app/src/main/java/freerider/datamodel/Customer.java
@@ -2,6 +2,13 @@ package freerider.datamodel;
 
 import java.util.*;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Enumerated;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+
 
 /**
  * Class for entity type Customer. Customer is an individual who acts as holder of a business relationship.
@@ -10,32 +17,40 @@ import java.util.*;
  * @version "0.1.0"
  * @author sgra64
  */
-
+@Entity
+@Table(name = "CUSTOMER")
 public class Customer {
 
     /**
      * id attribute, {@code < 0} invalid, can be set only once.
      */
+    @Id
+    @Column(name = "ID")
     private long id = -1;
 
     /**
      * surname, never null, mapped to "" when empty.
      */
+    @Column(name = "NAME")
     private String lastName = "";
 
     /**
      * none-surname name parts, never null, mapped to "" when empty.
      */
+    @Column(name = "FIRST_NAME")
     private String firstName = "";
 
     /**
      * contact information with multiple contact entries.
      */
+    @Transient
     private List<String> contacts = new ArrayList<String>();
 
     /**
      * status information of a Customer.
      */
+    @Column(name = "STATUS")
+    @Enumerated
     private Status status = Status.New;
 
 
